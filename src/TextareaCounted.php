97e486e022b29417fc7ea184f6dd43ced17f5e52
Patrick Corkery <patrick@thesmartgroup.ie>
2019-04-25 10:42:52 +0100

    add files

---
+++ src/TextareaCounted.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace ElevateDigital\CharcountedFields;
+
+class TextareaCounted extends FieldCounted
+{
+
+    /**
+     * The field's component.
+     * @var string
+     */
+    public $component = 'textarea-counted';
+
+}
